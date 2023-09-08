ed8776b73d6f291d7418fd1399c628e392081ccd
CarlosKyono <144291302+CarlosKyono@users.noreply.github.com>
2023-09-08 01:54:54 +0000

    Resultados de la clase

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Mascota.hpp>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "hola Mascota puerk" << std::endl;
+
+    Mascota m1("Dario Puerk");
+    m1.Jugar();
+    m1.Jugar();
+    m1.Jugar();  
+
+    std::cout<<m1.
+
+    m1.Comer(5);
+
+
+    return 0;
+}
